6c9f09494b3a459efac15f98b0fc40f74d71f127
vinceberg123 <166601399+vinceberg123@users.noreply.github.com>
2025-02-17 09:17:06 +0100

    Update Program.cs

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,6 +1,8 @@
-﻿using Quiz;
+using Quiz;
 
-/*Console.WriteLine("[?] Enter your username");
+RoundThree();
+// Introduction
+Console.WriteLine("[?] Enter your username");
 string userName = Console.ReadLine();
 
 if (string.IsNullOrEmpty(userName))
@@ -25,36 +27,34 @@ if (totalScoreRoundOne < 2)
 Console.WriteLine("[+] Succesfully passed round 1");
 
 // Round two
-Console.WriteLine("\n");
-Console.WriteLine("[?] Guess a number..");
+Console.WriteLine("\n[?] Guess a number..");
 GuessNumber.Run();
 
 // Round three
 Console.WriteLine("\n");
-RoundThree();*/
+RoundThree();
 
 // Round Four
 Console.WriteLine("\n[+] Keep in mind, the dog is going to run as much as you press the key. So please keep in mind to not hold down a key, instead just press it.");
 RunningDog.ShowRunningDog();
 
-
 // Round five
 Console.WriteLine("\n");
 int totalScoreRoundFive = RoundFive();
 
-if (totalScoreRoundFive < 2)
+if (totalScoreRoundFive < 1)
 {
-    Console.WriteLine("[-] Failed to pass round 1");
+    Console.WriteLine("[-] Failed to pass round 5");
     return;
 }
 
-Console.WriteLine("[+] Finished quiz. (Yeey)");
+Console.WriteLine("[+] Finished quiz.");
 
 static int RoundOne()
 {
-    int firstQuestion = AskQuestion("[?] windows or linux?", "linux");
+    int firstQuestion = AskQuestion("[?] Who was created first windows or linux?", "windows");
     int secondQuestion = AskQuestion("[?] What code editor did microsoft create, xcode or visual studio", "visual studio");
-    int thirdQuestion = AskQuestion("[?] What year is linux found? ", "1991");
+    int thirdQuestion = AskQuestion("[?] What year was linux created? ", "1991");
 
     int totalScoreRound = firstQuestion + secondQuestion + thirdQuestion;
 
@@ -63,9 +63,9 @@ static int RoundOne()
 
 static void RoundThree()
 {
-    AskQuestionWithoutAnswer("[?] Why is it important to update your computer reguraly?");
-    AskQuestionWithoutAnswer("[?] What does a cpu do in a computer?");
-    AskQuestionWithoutAnswer("[?] What to do when your computer is empty?");
+    AskQuestionWithoutAnswer("[?] Why is it important to update your computer reguraly?", "\n[ ] because else your computer doesn't get the latest security updates, and its vulnarable for hackers.");
+    AskQuestionWithoutAnswer("[?] What does a cpu do in a computer?", "\n[ ] a cpu is the main part of a computer that processes instructions from programs, performing calculations and managing data flow. It acts as the computers brain executing tasks through its cores and threads to keep the system running smoothly.");
+    AskQuestionWithoutAnswer("[?] What to do when your laptop battery is empty?", "\n[ ] charge the laptop");
 }
 
 static int RoundFive()
@@ -96,8 +96,9 @@ static int AskQuestion(string question, string answer)
     return 0;
 }
 
-static void AskQuestionWithoutAnswer(string question)
+static void AskQuestionWithoutAnswer(string question, string answer)
 {
     Console.WriteLine(question);
     Console.ReadLine();
+    Console.WriteLine($"The right answer contains, {answer}");
 }
