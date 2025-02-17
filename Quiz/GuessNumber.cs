03059317a12bf28a68399134a8c341abd43b7e45
vinceberg123 <166601399+vinceberg123@users.noreply.github.com>
2025-02-17 09:16:07 +0100

    init commit

---
+++ Quiz/GuessNumber.cs
@@ -0,0 +1,34 @@
+﻿namespace Quiz
+{
+    internal class GuessNumber
+    {
+        public static void Run()
+        {
+            bool isNumberGuessed = false;
+
+            Random random = new Random();
+            int randomNumber = random.Next(100);
+
+            while (!isNumberGuessed)
+            {
+                int guessedNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (guessedNumber == randomNumber)
+                {
+                    Console.WriteLine($"[+] Succesfully guessed number {randomNumber} was indeed the number");
+                    return;
+                }
+
+                if (guessedNumber > randomNumber)
+                {
+                    Console.WriteLine("[-] guess lower");
+                }
+
+                if (guessedNumber < randomNumber)
+                {
+                    Console.WriteLine("[+] guess higher");
+                }
+            }
+        }
+    }
+}
---
+++ Quiz/RunningDog.cs
@@ -0,0 +1,59 @@
+﻿namespace Quiz
+{
+    internal class RunningDog
+    {
+        public static void ShowRunningDog()
+        {
+            string baseDog = @"   
+                  ;~~,__
+   :-....,-------'`-'._.'
+    `-,,,  ,       ,'~~'
+        ; ,'~.__; /
+        :|      :|
+        `-'     `-'";
+
+            string firstRunningDog = @"
+                  ;~~,__
+   :-....,-------'`-'._.'
+    `-,,,  ,       ,'~~'
+        ; ,'~.__; /--.
+        :| :|   :|``(;
+        `-'`-'  `-'";
+
+            string secondRunningDog = @"
+                  ;~~,__
+   :-....,-------'`-'._.'
+    `-,,,  ,       ;'~~'
+       ,'_,'~.__; '--.
+      //'       ````(;
+     `-'";
+
+            string thirdRunningDog = @"
+                .--~~,__
+   :-....,-------`~~'._.'
+    `-,,,  ,_      ;'~U'
+     _,-' ,'`-__; '--.
+    (_/'~~      ''''(;";
+
+
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                AnimateDog(baseDog);
+                AnimateDog(firstRunningDog);
+                AnimateDog(secondRunningDog);
+                AnimateDog(thirdRunningDog);
+
+                Console.WriteLine("[ ] Press enter to quit. Or any other key to let de dog run.");
+            }
+
+            Console.Clear();
+        }
+
+        public static void AnimateDog(string animation)
+        {
+            Console.Clear();
+            Thread.Sleep(200);
+            Console.WriteLine(animation);
+        }
+    }
+}
